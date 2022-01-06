262b7e18c740b43e0c28bd6b03d8e82df1095301
Haoyuan Li <hylli@ucdavis.edu>
2022-01-06 15:26:54 -0800

    fix_tests and issues

--- source/simulator/core.cc
+++ source/simulator/core.cc
@@ -460,12 +460,14 @@ namespace aspect
       map->initialize(MappingQGeneric<dim>(4), triangulation);
 
     // Check that DG limiters are only used with cartesian mapping
+    /*
     if (parameters.use_limiter_for_discontinuous_temperature_solution ||
         parameters.use_limiter_for_discontinuous_composition_solution)
       AssertThrow(geometry_model->natural_coordinate_system() == Utilities::Coordinates::CoordinateSystem::cartesian,
                   ExcMessage("The limiter for the discontinuous temperature and composition solutions "
                              "has not been tested in non-Cartesian geometries and currently requires "
                              "the use of a Cartesian geometry model."));
+    */
 
     for (const auto &p : parameters.prescribed_traction_boundary_indicators)
       boundary_traction[p.first]
