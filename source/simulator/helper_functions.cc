ee9e0d5c3eee65b68b14fbd1aae17e1d0102aeb9
Haoyuan Li <hylli@ucdavis.edu>
2026-04-15 10:15:07 -0700

    follow

--- source/simulator/helper_functions.cc
+++ source/simulator/helper_functions.cc
@@ -2141,11 +2141,11 @@ namespace aspect
                                "but the `adiabatic heating' plugin has not been selected in the input file. "
                                "Please check the consistency of your input file."));
 
-        AssertThrow(heating_model_manager.shear_heating_enabled(),
-                    ExcMessage("ASPECT detected an inconsistency in the provided input file. "
-                               "The `isentropic compression' formulation expects shear heating to be enabled, "
-                               "but the `shear heating' plugin has not been selected in the input file. "
-                               "Please check the consistency of your input file."));
+        // AssertThrow(heating_model_manager.shear_heating_enabled(),
+        //             ExcMessage("ASPECT detected an inconsistency in the provided input file. "
+        //                        "The `isentropic compression' formulation expects shear heating to be enabled, "
+        //                        "but the `shear heating' plugin has not been selected in the input file. "
+        //                        "Please check the consistency of your input file."));
       }
     else if (parameters.formulation == Parameters<dim>::Formulation::boussinesq_approximation)
       {
@@ -2155,11 +2155,11 @@ namespace aspect
                                "but the `adiabatic heating' plugin has been selected in the input file. "
                                "Please check the consistency of your input file."));
 
-        // AssertThrow(!heating_model_manager.shear_heating_enabled(),
-        //             ExcMessage("ASPECT detected an inconsistency in the provided input file. "
-        //                        "The 'Boussinesq approximation' formulation expects shear heating to be disabled, "
-        //                        "but the `shear heating' plugin has been selected in the input file. "
-        //                        "Please check the consistency of your input file."));
+        AssertThrow(!heating_model_manager.shear_heating_enabled(),
+                    ExcMessage("ASPECT detected an inconsistency in the provided input file. "
+                               "The 'Boussinesq approximation' formulation expects shear heating to be disabled, "
+                               "but the `shear heating' plugin has been selected in the input file. "
+                               "Please check the consistency of your input file."));
       }
     else if (parameters.formulation == Parameters<dim>::Formulation::anelastic_liquid_approximation)
       {
